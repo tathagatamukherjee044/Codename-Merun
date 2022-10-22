940aa389ae812f5b01048447b3da1a7906be7e65
tathagata <tathagata.mukherjee044@gmail.com>
2022-10-22 19:12:41 +0530

    fixed linking system and changed train sprites

--- Assets/Scripts/Views/TrainGeneratorView.cs
+++ Assets/Scripts/Views/TrainGeneratorView.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class TrainGeneratorView : MonoBehaviour
 {
     public GameObject train;
-    public float pos = 7.5f;
+    //public float pos = 7.5f;
     public GameObject previous;
     public HingeJoint2D hinge;
     // Start is called before the first frame update
@@ -23,20 +23,14 @@ public class TrainGeneratorView : MonoBehaviour
     
     private void OnTriggerExit2D(Collider2D other) {
         //Debug.Log("train has left");
-        Debug.Log(other.gameObject.transform.position);
-        Instantiate(train,new Vector3 (pos, -2, 0), Quaternion.identity);
+        float pos = other.gameObject.transform.position.x;
+        Instantiate(train,new Vector3 (pos+17.6f, 1.36f, 0), Quaternion.identity);
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("entered");
-        Debug.Log(other);
-        Debug.Log("previous");
-        Debug.Log(previous);
         hinge = other.gameObject.GetComponent<HingeJoint2D>();
         hinge.enabled = true;
         hinge.connectedBody = previous.GetComponent<Rigidbody2D>();
         previous = other.gameObject;
-        Debug.Log("now previous is ");
-        Debug.Log(previous);
     }
 }
